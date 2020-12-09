f617d4be71f00790698eb4a5e21439d5fe3970f7
Sean Biller <seanmbiller@gmail.com>
2020-12-09 18:20:35 -0500

    finished app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
+import Translate from "./components/Translate";
+import Route from "./components/Route";
+import Header from "./components/Header";
 
 const items = [
   {
@@ -18,10 +21,43 @@ const items = [
   }
 ];
 
+const options = [
+  {
+    label: "The Color Red",
+    value: "red"
+  },
+  {
+    label: "The Color Green",
+    value: "green"
+  },
+  {
+    label: "The Color Blue",
+    value: "blue"
+  }
+];
+
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
-      <Dropdown />
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 };
